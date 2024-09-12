1d50e72e27cafe58eaedf4088a4da588f2e01918
Aashutosh Dubey <96036212+Aashutosh16@users.noreply.github.com>
2024-09-12 23:02:30 +0530

    Time: 7 ms (8.24%) | Memory: 11.2 MB (31.99%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
-    int hIndex(vector<int>& citations) {
-        sort(citations.begin(), citations.end(), greater<int>());
-        int h = 0;
-        for(int i=0; i<citations.size();++i){
-            if(citations[i] >= i+1){
-                h = i + 1;
-            }
-            else{
-                break;
-            }
-        }
-        return h;
+    int hIndex(vector<int>& citations){
+        int n=citations.size();
+        sort(citations.begin(),citations.end());
+        for(int i=0;i<n;i++) if((n-i)<=citations[i]) return (n-i);
+        return 0;
     }
 };
