981694531e474bc66e2c16ee4e329deef3e8c098
Aashutosh Dubey <96036212+Aashutosh16@users.noreply.github.com>
2024-09-16 23:23:28 +0530

    Time: 97 ms (51.70%) | Memory: 111.2 MB (85.82%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int total_gas = 0, total_cost = 0;
-        int current_gas = 0, start_index = 0;
-
-        for (int i = 0; i < gas.size(); ++i) {
-            total_gas += gas[i];
-            total_cost += cost[i];
-            current_gas += gas[i] - cost[i];
-            
-            // If current gas falls below 0, reset the start station
-            if (current_gas < 0) {
-                start_index = i + 1;
-                current_gas = 0; // Reset current gas for the new start
-            }
-        }
-
-        // If total gas is less than total cost, it's impossible to complete the circuit
-        if (total_gas < total_cost) {
-            return -1;
-        }
-
-        return start_index;
+       int total_gas = 0 , current_gas = 0, start_index = 0;
+       for (int i = 0; i < gas.size();i++){
+        int net_gas = gas[i] - cost[i];
+        total_gas += net_gas;
+        current_gas += net_gas;
+        if(current_gas < 0){
+            start_index = i + 1;
+            current_gas = 0;
+        } 
+       }
+       return total_gas >= 0 ? start_index : -1;
     }
 };
