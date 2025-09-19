43269c74690f97c4a3442c053643fbc56154f269
Aashutosh Dubey <96036212+Aashutosh16@users.noreply.github.com>
2025-09-19 23:21:40 +0530

    Time: 0 ms (100.00%) | Memory: 52.1 MB (88.42%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
-        int maxReach=0;
-        for (int i =0;i<n;++i){
-            if(i>maxReach){
-                return false;
-            }
-            maxReach= max(maxReach ,i + nums[i]);
-            if (maxReach >=n-1){
-                return true;
-            }
+        int maxReach = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i > maxReach) return false; // can't reach this index
+            maxReach = max(maxReach, i + nums[i]);
         }
-        return false;
+        return true;
     }
 };
